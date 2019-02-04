15ba4ebcd428e5614a7f38cbba301993494c81ec
warzesia <magdalena.warzecha@websudos.co.uk>
2019-02-04 19:16:28 +0100

    added handling bad server response to Rest Connection Handler

--- src/main/java/APITestingAutomation/Main.java
+++ src/main/java/APITestingAutomation/Main.java
@@ -1,18 +1,11 @@
 package APITestingAutomation;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-import java.util.Properties;
-
 import org.json.JSONObject;
-
 import helpers.Constants;
 import helpers.JSONHelper;
 import helpers.RESTConnectionHandler;
 import responses.FlexScoreResponse;
-import responses.PtuDatum;
 
 
 public class Main 
@@ -28,35 +21,35 @@ public class Main
         		JSONHelper.parseJSONObjectFromFile(Constants.VALID_RESPONSE_JSON_PATH_1);
 
 
-//        JSONObject responseJSONObject;
-//        int responseCode;
-//        
-//		RESTConnectionHandler connectionHandler = new RESTConnectionHandler();
-//		connectionHandler.openHTTPConnection(Constants.API_ENDPOINT_URL_FLEX_REQUEST);
-//		connectionHandler.sendPOSTRequest(exampleValidJsonRequest.toString());
-//		responseCode = connectionHandler.getResponseCode();
-//		responseJSONObject = connectionHandler.getResponseBody();
-//		connectionHandler.closeHTTPConnection();
-//		
-//		System.out.println(responseCode);
-//		System.out.println(responseJSONObject);
+        JSONObject responseJSONObject;
+        int responseCode;
+        
+		RESTConnectionHandler connectionHandler = new RESTConnectionHandler();
+		connectionHandler.openHTTPConnection(Constants.API_ENDPOINT_URL_FLEX_REQUEST);
+		connectionHandler.sendPOSTRequest(exampleValidJsonRequest.toString());
+		responseCode = connectionHandler.getResponseCode();
+		responseJSONObject = connectionHandler.getResponseBody();
+		connectionHandler.closeHTTPConnection();
+		
+		System.out.println(responseCode);
+		System.out.println(responseJSONObject);
 		
         
-		FlexScoreResponse flexScoreResponse = 
-				JSONHelper.parseFlexScoreResponseFromJSONObject(exampleValidJsonResponse);
-		
-		System.out.println(flexScoreResponse.flexScore.totalScore);
-		System.out.println(flexScoreResponse.flexScore.offerScore);
-		flexScoreResponse.flexScore.ptuData.forEach(ptuDatum->
-			System.out.println(
-					"Start: " + ptuDatum.start + ", " +
-					"Power: " + ptuDatum.power + ", " +
-					"Price: " + ptuDatum.price + ", " +
-					"Disposition: " + ptuDatum.disposition + ", " +
-					"powerScore: " + ptuDatum.powerScore + ", " +
-					"priceScore: " + ptuDatum.priceScore)
-				);
-
-		System.out.println(flexScoreResponse.validateAgainstRules());
+//		FlexScoreResponse flexScoreResponse = 
+//				JSONHelper.parseFlexScoreResponseFromJSONObject(exampleValidJsonResponse);
+//		
+//		System.out.println(flexScoreResponse.flexScore.totalScore);
+//		System.out.println(flexScoreResponse.flexScore.offerScore);
+//		flexScoreResponse.flexScore.ptuData.forEach(ptuDatum->
+//			System.out.println(
+//					"Start: " + ptuDatum.start + ", " +
+//					"Power: " + ptuDatum.power + ", " +
+//					"Price: " + ptuDatum.price + ", " +
+//					"Disposition: " + ptuDatum.disposition + ", " +
+//					"powerScore: " + ptuDatum.powerScore + ", " +
+//					"priceScore: " + ptuDatum.priceScore)
+//				);
+//
+//		System.out.println(flexScoreResponse.validateAgainstRules());
     }
 }
--- src/main/java/helpers/RESTConnectionHandler.java
+++ src/main/java/helpers/RESTConnectionHandler.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
@@ -12,31 +14,53 @@ public class RESTConnectionHandler {
 	
 	HttpURLConnection connection;
 	
-	public void openHTTPConnection(String endpointURL) throws IOException {
-		URL url = new URL(endpointURL);
-        this.connection = (HttpURLConnection) url.openConnection();
-        this.connection.setConnectTimeout(5000);
-        this.connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-        this.connection.setDoOutput(true);
-        this.connection.setDoInput(true);
+	public void openHTTPConnection(String endpointURL){
+		try {
+			URL url = new URL(endpointURL);
+			this.connection = (HttpURLConnection) url.openConnection();
+	        this.connection.setConnectTimeout(5000);
+	        this.connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+	        this.connection.setDoOutput(true);
+	        this.connection.setDoInput(true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void sendPOSTRequest(String jsonBody) throws IOException {
-		this.connection.setRequestMethod("POST");
-        OutputStream os = this.connection.getOutputStream();
-        os.write(jsonBody.getBytes("UTF-8"));
-        os.close(); 
+	public void sendPOSTRequest(String jsonBody){
+		try {
+			this.connection.setRequestMethod("POST");
+	        OutputStream os = this.connection.getOutputStream();
+	        os.write(jsonBody.getBytes("UTF-8"));
+	        os.close(); 
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
-	public int getResponseCode() throws IOException {
-		return this.connection.getResponseCode();
+	public int getResponseCode(){
+		try {
+			return this.connection.getResponseCode();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return 0;
+		}
 	}
 	
-	public JSONObject getResponseBody() throws IOException {
-        InputStream in = new BufferedInputStream(this.connection.getInputStream());
-        String result = IOUtils.toString(in, "UTF-8");
-        in.close();
-        return new JSONObject(result);
+	public JSONObject getResponseBody(){
+		try {
+	        InputStream in = new BufferedInputStream(this.connection.getInputStream());
+	        String result;
+			result = IOUtils.toString(in, "UTF-8");
+	        in.close();
+	        return new JSONObject(result);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 	public void closeHTTPConnection() {
